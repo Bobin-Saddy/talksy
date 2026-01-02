61e79662013d4f6e375ba09b7aa5ec474f3b8ce9
Digittrix Dev <dev@digittrix.in>
2026-01-02 16:44:44 +0530

    cdc

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -20,7 +20,7 @@ export default function App() {
       <PolarisAppProvider i18n={enTranslations}>
         <s-app-nav>
           {/* <s-link href="/app">Home</s-link> */}
-          <s-link href="/app/chat/admin">Chat page</s-link>
+          <s-link href="/app/chat/admin">Chats</s-link>
           <s-link href="/app/settings">Settings</s-link>
         </s-app-nav>
         <Outlet />
