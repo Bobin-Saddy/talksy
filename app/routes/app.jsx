0e403851c4afa316a34f9a3074cf6fcc21b2439d
Digittrix Dev <dev@digittrix.in>
2026-02-06 16:28:51 +0530

    cdc

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -7,7 +7,7 @@ import enTranslations from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
 import { authenticate } from "../shopify.server";
 import { useEffect } from "react";
-import { getUsageStats } from "../utils/planLimits.server";
+import { getUsageStats } from "./app/utils/planLimits.server";
 
 export const loader = async ({ request }) => {
   const { session } = await authenticate.admin(request);
