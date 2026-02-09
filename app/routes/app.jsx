bd7ba82f5748126c1a22e996761a7ae8f60d12cd
Digittrix Dev <dev@digittrix.in>
2026-02-09 18:19:47 +0530

    update-this

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,5 +1,5 @@
 // app/routes/app.jsx - COMPLETE FIXED VERSION
-import { Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider as ShopifyAppProvider } from "@shopify/shopify-app-react-router/react";
 import { AppProvider as PolarisAppProvider, Badge } from "@shopify/polaris";
