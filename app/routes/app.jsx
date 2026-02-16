4d61e4fcc1a927619c54fd4cc1a68630d9c39754
Digittrix Dev <dev@digittrix.in>
2026-02-16 12:35:17 +0530

    update-this

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -58,7 +58,7 @@ export const loader = async ({ request }) => {
 function LockedPageOverlay({ requiredPlan, currentPlan, isPendingApproval, onNavigate }) {
   return (
     <div style={{
-      position: "absolute",
+      position: "fixed",
       top: 0,
       left: 0,
       right: 0,
