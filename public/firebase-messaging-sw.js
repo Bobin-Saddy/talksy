5304b9d4384ea2b553a633342934f39e03bd8b3c
Digittrix Dev <dev@digittrix.in>
2026-02-18 13:10:24 +0530

    cdc

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,5 @@
 // ============================================================
 // firebase-messaging-sw.js
-
 // ============================================================
 
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
